b8e951a0f97eebce59c301fb745dd28d4e7eba3e
Caden Ko <cadenjko@gmail.com>
2024-12-06 14:05:56 -0800

    Minor content changes.

--- db/migrate/20091206054953_add_scenerio_to_chambers.rb
+++ db/migrate/20091206054953_add_scenerio_to_chambers.rb
@@ -1,9 +1,9 @@
 class AddScenerioToChambers < ActiveRecord::Migration[4.2]
   def self.up
-    add_column :chambers, :scenerio, :string, :default => 'us_house_of_representatives'
+    add_column :chambers, :scenario, :string, :default => 'us_house_of_representatives'
   end
 
   def self.down
-    remove_column :chambers, :scenerio
+    remove_column :chambers, :scenario
   end
 end
